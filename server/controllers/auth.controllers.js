e07ce97f9145b8457285ce2918b337393f198608
Nitin Dogra <dogranitin579@gmail.com>
2026-04-23 22:42:20 +0530

    user password hashed validation

--- server/controllers/auth.controllers.js
+++ server/controllers/auth.controllers.js
@@ -1,3 +1,4 @@
+import { success } from 'zod';
 import User from '../models/user.model.js';
 import { registerSchema } from '../schemas/user.schema.js';
 import bcrypt from 'bcrypt';
@@ -10,10 +11,16 @@ const signUpController = async (req, res) => {
       return res.status(400).json({ success: false, message: error });
     }
     const { email, username, password } = result.data;
-    const hashedPassword = await bcrypt.hash(
-      password,
-      Number(process.env.SALT_ROUND)
-    );
+    const userExists = await User.findOne({ $or: [{ email, username }] });
+    if (userExists)
+      return res.status(409).json({
+        success: false,
+        message: 'user already exists',
+      });
+    const saltRounds = Number(process.env.SALT_ROUNDS);
+    if (isNaN(saltRounds))
+      return res.status(500).json({ success: false, message: 'server broke' });
+    const hashedPassword = await bcrypt.hash(password, saltRounds);
     await User.create({ email, username, password: hashedPassword });
     res
       .status(201)
@@ -21,9 +28,9 @@ const signUpController = async (req, res) => {
   } catch (err) {
     if (err.code === 11000)
       return res
-        .status(400)
+        .status(409)
         .json({ success: false, message: 'user already exists' });
-    console.log(err);
+    console.error(err);
     res.status(500).json({ success: false, message: 'internal server error' });
   }
 };
